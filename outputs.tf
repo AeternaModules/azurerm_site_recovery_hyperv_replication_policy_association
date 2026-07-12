817cbc07836ec9ff0f5dac94b202ae1756816e52
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "site_recovery_hyperv_replication_policy_associations" {
-  description = "All site_recovery_hyperv_replication_policy_association resources"
-  value       = azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations
-}
 output "site_recovery_hyperv_replication_policy_associations_hyperv_site_id" {
-  description = "List of hyperv_site_id values across all site_recovery_hyperv_replication_policy_associations"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : v.hyperv_site_id]
+  description = "Map of hyperv_site_id values across all site_recovery_hyperv_replication_policy_associations, keyed the same as var.site_recovery_hyperv_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : k => v.hyperv_site_id }
 }
 output "site_recovery_hyperv_replication_policy_associations_name" {
-  description = "List of name values across all site_recovery_hyperv_replication_policy_associations"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : v.name]
+  description = "Map of name values across all site_recovery_hyperv_replication_policy_associations, keyed the same as var.site_recovery_hyperv_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : k => v.name }
 }
 output "site_recovery_hyperv_replication_policy_associations_policy_id" {
-  description = "List of policy_id values across all site_recovery_hyperv_replication_policy_associations"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : v.policy_id]
+  description = "Map of policy_id values across all site_recovery_hyperv_replication_policy_associations, keyed the same as var.site_recovery_hyperv_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : k => v.policy_id }
 }
 
