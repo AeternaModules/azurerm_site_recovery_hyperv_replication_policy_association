c85741a7297fdfea3c994b24f9162f48bfaac2d7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:05:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "site_recovery_hyperv_replication_policy_associations_id" {
+  description = "Map of id values across all site_recovery_hyperv_replication_policy_associations, keyed the same as var.site_recovery_hyperv_replication_policy_associations"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : k => v.id }
+}
 output "site_recovery_hyperv_replication_policy_associations_hyperv_site_id" {
   description = "Map of hyperv_site_id values across all site_recovery_hyperv_replication_policy_associations, keyed the same as var.site_recovery_hyperv_replication_policy_associations"
   value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy_association.site_recovery_hyperv_replication_policy_associations : k => v.hyperv_site_id }
